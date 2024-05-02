4d96b4c4b61cf657a9c2dc5e2532d6566bbec967
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-05-02 14:31:43 -0700

    Edit: Trying a different approach

--- conversorDivisas/src/com/aluracursos/conversorDivisas/main/Main.java
+++ conversorDivisas/src/com/aluracursos/conversorDivisas/main/Main.java
@@ -4,6 +4,43 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner  scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+    
+    String welcomeMsg = "\n Bienvenido(a) al conversor de monedas \n";
+
+    String menu = """
+        1 - USD a MXN
+        2 - MXN a USD
+        3 - USD a PER
+        4 - PER a USD
+        5 - USD a ARG
+        6 - ARG a USD
+        7 - Más opciones de conversión
+        8 - Historial de conversiones
+        0 - Salir
+        """;
+
+    String byeMessage = "\n Gracias por su visita \n";
+
+    System.out.println(welcomeMsg);
+
+    var terminar = false;
+
+    while (true) {
+      System.out.println(menu);
+
+      try {
+        var opcion = scanner.nextInt();
+
+        if (opcion == 0) {
+          break;
+        }
+
+      } catch (Exception e) {
+        // TODO: handle exception
+      }
+    }
+    
+    System.out.println(byeMessage);
   }
 }
--- conversorDivisas/src/com/aluracursos/conversorDivisas/models/ExchangeRateResponse.java
+++
@@ -1,8 +0,0 @@
-package com.aluracursos.conversorDivisas.models;
-
-import java.util.Map;
-
-import com.google.gson.JsonObject;
-
-public record ExchangeRateResponse(String base_code, JsonObject conversion_rates) {
-}
