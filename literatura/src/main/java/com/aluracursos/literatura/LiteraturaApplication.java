a667cfaea63289106956fd8b4deb1a6f95e43a65
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-05-24 16:02:19 -0700

    Feature: Added dotenv configuration to load database properties from environment variables

--- literatura/src/main/java/com/aluracursos/literatura/LiteraturaApplication.java
+++ literatura/src/main/java/com/aluracursos/literatura/LiteraturaApplication.java
@@ -3,10 +3,18 @@ package com.aluracursos.literatura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LiteraturaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(LiteraturaApplication.class, args);
 	}
 
