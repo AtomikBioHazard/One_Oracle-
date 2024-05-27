ceaff692192605ee86cae986a772051ebc9690a1
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-05-27 16:30:29 -0700

    Updating menu: Lists all the possible options

--- literatura/src/main/java/com/aluracursos/literatura/principal/Principal.java
+++ literatura/src/main/java/com/aluracursos/literatura/principal/Principal.java
@@ -10,8 +10,23 @@ public class Principal {
 
     while (option != 9) {
       var myMenu = """
-          *** Literalura ***
+          *** *** *** *** *** *** *** ***
+                    Literalura
+          *** *** *** *** *** *** *** ***
+
+          1 - Consultar Libro por Titulo
+          2 - Consultar Autor por Nombre
+          3 - Listar Libros Registrados
+          4 - Listar Autores Registrados
+          5 - Listar Autores Vivos por año
+          6 - Listar Libros por Idioma
+          7 - Top 10 Libros más Buscados
+          8 - Generar Estadísticas
+
           9 - Salir
+          *** *** *** *** *** *** *** ***
+
+          Elige una opción: 
           """;
 
       System.out.println(myMenu);
