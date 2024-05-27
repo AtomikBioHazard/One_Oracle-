390e5a3759b75cc3160505075800118986015074
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-05-27 16:14:50 -0700

    Fix: fixes dotenv configuration error, and Updates override method run to run the app

--- literatura/src/main/java/com/aluracursos/literatura/LiteraturaApplication.java
+++ literatura/src/main/java/com/aluracursos/literatura/LiteraturaApplication.java
@@ -1,21 +1,31 @@
 package com.aluracursos.literatura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.aluracursos.literatura.principal.Principal;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-public class LiteraturaApplication {
+public class LiteraturaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		String envileFilePath = "/literatura/.env";
+		Dotenv dotenv = Dotenv.configure().filename(envileFilePath).load();
 		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
 		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.menu();
 	}
 
 }
