b470115f6833f2ab9d7fe69011275ffbc936d7d5
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-07-10 14:58:58 -0700

    Topic registration record created

---
+++ foro/src/main/java/com/alurachallenge/foro/dto/DatosRegistroTopico.java
@@ -0,0 +1,20 @@
+package com.alurachallenge.foro.dto;
+
+import com.alurachallenge.foro.model.Curso;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosRegistroTopico(
+  @NotBlank
+  String titulo,
+
+  @NotBlank
+  String mensaje,
+
+  @NotNull @Valid
+  Curso curso
+) {
+
+}
