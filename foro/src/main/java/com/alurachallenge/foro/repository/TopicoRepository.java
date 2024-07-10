5f17413998545a423fc84353fc5dfa0f76df7f9a
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-07-10 14:56:12 -0700

    Topico repository created

---
+++ foro/src/main/java/com/alurachallenge/foro/repository/TopicoRepository.java
@@ -0,0 +1,9 @@
+package com.alurachallenge.foro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alurachallenge.foro.model.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
