0032c157b551ee4c962c642c92058e93f3c78339
AtomikBioHazard <atomikbiohazard@gmail.com>
2024-07-12 16:43:23 -0700

    Bugfix: fixing search error

--- foro/src/main/java/com/alurachallenge/foro/repository/UsuarioRepository.java
+++ foro/src/main/java/com/alurachallenge/foro/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.alurachallenge.foro.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-  Usuario findByLogin(String login);
+  Usuario findByUsername(String username);
 }
